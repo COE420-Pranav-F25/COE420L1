9770a909604dbb2a7b597bde9c89121be45fdadc
makinlemonade <lemonadegrenade@protonmail.com>
2025-09-03 17:50:55 +0400

    Feature B added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld {
+    public static void FeatureB() {
+        System.out.println("This is feature B");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        FeatureB();
     }
 }
-
